b3412e1fc6ef0ecc098efa67936829a601f3c952
martinslemosana <martins.ana@unifesp.br>
2026-05-14 20:41:50 -0300

    Resolução vetores e matrizes

---
+++ lista-vetores-matrizes/m10.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+#define N 3
+
+int main(){
+  int m[N][N], soma=0;
+
+  srand(time(NULL));
+  for(int i=0;i<N;i++){
+    for(int j=0;j<N;j++){
+      m[i][j] = rand()%10;
+    }
+  }
+
+  printf("\nMatriz: \n");
+  for(int i=0;i<N;i++){
+    for(int j=0;j<N;j++){
+      printf("%d\t",m[i][j]);
+    }
+    printf("\n");
+  }
+
+  for(int i=1;i<N;i++){
+    soma+=m[i][i];
+  }
+  printf("\nSoma: %d\n",soma);
+}
